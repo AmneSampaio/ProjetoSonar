123bc428c6b380a3a8c550abfb28fa7d3501abee
AmneSampaio <amnesf@gmail.com>
2022-02-17 13:10:46 -0300

    Initial commit

---
+++ src/main/kotlin/measurementDeepSonar.kt
@@ -0,0 +1,17 @@
+import java.io.File
+
+class measurementDeepSonar {
+
+    fun main() {
+        var count = 0
+        val lineList = mutableListOf<String>()
+
+        File("C:/Users/amne.sampaio.fredo/OneDrive - Accenture/Documents/cursoKotlin_Julio/Puzzle/Puzzle_01.txt").useLines { lines -> lines.forEach { lineList.add(it) }}
+
+        for (x in 1 until lineList.size){
+
+            
+        }
+        println(count)
+    }
+}
